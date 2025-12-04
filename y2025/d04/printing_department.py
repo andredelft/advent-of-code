7840dbaaf5d48b0d95007cf67a56928681f11c7f
André van Delft <andrevandelft@outlook.com>
2025-12-04 14:15:24 +0100

    2025, day 4

---
+++ y2025/d04/printing_department.py
@@ -0,0 +1,33 @@
+from lib.field import Field, Coordinate
+import time
+
+
+def is_accessible(coord: Coordinate, field: Field):
+    return sum(field[neighbour] == "@" for neighbour in field.coords_around(*coord)) < 4
+
+
+def solve_a(input_string: str):
+    field = Field(input_string)
+
+    return sum(
+        is_accessible(coord, field) for coord, char in field.enumerate() if char == "@"
+    )
+
+
+def solve_b(input_string: str):
+    field = Field(input_string)
+    num_rolls = sum(char == "@" for char in field)
+
+    while True:
+        new_field = field.copy()
+
+        for coord, char in field.enumerate():
+            if char == "@" and is_accessible(coord, field):
+                new_field[coord] = "."
+
+        if field == new_field:
+            break
+
+        field = new_field
+
+    return num_rolls - sum(char == "@" for char in field)
---
+++ y2025/d04/test_printing_department.py
@@ -0,0 +1,38 @@
+import pytest
+from y2025.d04.printing_department import solve_a, solve_b
+
+
+TEST_INPUT = """\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+
+@pytest.fixture
+def test_input():
+    return TEST_INPUT
+
+
+@pytest.fixture
+def expected_solution_a():
+    return 13
+
+
+@pytest.fixture
+def expected_solution_b():
+    return None
+
+
+def test_solve_a(test_input, expected_solution_a):
+    assert solve_a(test_input) == expected_solution_a
+
+
+def test_solve_b(test_input, expected_solution_b):
+    assert solve_b(test_input) == expected_solution_b
